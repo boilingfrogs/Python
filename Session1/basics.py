059dae056915c1797680ba255f18c85b500a56bc
Boilingfrogs <boilingfrogs2015@gmail.com>
2020-05-17 03:29:19 +0530

    Session1-Files

---
+++ Session1/basics.py
@@ -0,0 +1,22 @@
+# Hash is used to comment out the lines
+# Pycharm shortcuts
+# To comment(uncomment) out any line(s)    :   ctrl + / (forward slash)
+# To run the program                       :   ctrl + shift + F10
+# print("Hello World")
+
+# msg = 'Hello "How are you"'
+# print(msg)
+
+# Indexing and Slicing
+# Indexing ==> [ ] ==> Square bracket ==> Subscript Operator
+# Indexing always starts from 0 (zero) to Length - 1
+# Negative Indexing ==> -length to -1
+# Slicing ==> To extract a substring out of a string []
+# Slicing ==> [start : end] ==> [included : excluded]
+
+# B  O  I  L  I  N  G  F  R  O  G  S
+# 0  1  2  3  4  5  6  7  8  9  10 11 ==> (+ve indexing)
+# 12 11 10 9  8  7  6  5  4  3  2  1  ==> (-ve indexing)
+
+# r ==> RAW String
+# Treat the string as it is. Don't escape any characters.
