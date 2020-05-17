9a700b13bf6621cd57abada2d03927edd98f7e57
Boilingfrogs <boilingfrogs2015@gmail.com>
2020-05-18 00:49:04 +0530

    Session2-Files

---
+++ Session2/dt_numbers.py
@@ -0,0 +1,22 @@
+# integers
+# floats
+
+# Operations supported
+# Addition          +
+# Subtraction       -
+# Multiplication    *
+# Division          /
+# Modulus           %
+# Floor             //
+# Power             ** , pow()
+
+
+# Boolean
+# False True
+
+# Equals                ==
+# Not Equal             !=
+# Greater than          >
+# Greater than equal to >=
+# Less than             <
+# Less than equal to    <=
