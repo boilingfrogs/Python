eeffeeb64ee32e57324231eb6f589d5b7008770f
Boilingfrogs <boilingfrogs2015@gmail.com>
2020-05-18 21:32:07 +0530

    Session3-Files

---
+++ Session3/dt_lists.py
@@ -0,0 +1,20 @@
+# Lists
+# Ordered Collection of objects/items
+# Syntax : [ ]
+# Mutable
+
+# Operations
+# append        ==> Adds an element at the end of list
+# insert        ==> Adds an element at a specified position
+
+# remove        ==> Removes an element using name/value
+# pop           ==> Removes an element using index
+
+# Indexing / Slicing
+
+# extend       ==> extends the list by the number of elements present in the container
+
+# MUTABLE      ==> List can be modified
+# IMMUTABLE    ==> Example: Strings, once defined, it can't be modified
+
+# * can also work as repetition operator
